87de9da3bde4b20c2e3e157cd0aa05e227f47c88
Qhien78 <hien3728195@gmail.com>
2025-03-11 10:57:55 +0700

    book

---
+++ BookCart/Controllers/BookController.cs
@@ -0,0 +1,62 @@
+﻿using BookCart.Data;
+using BookCart.Dto;
+using BookCart.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookCart.Controllers
+{
+    public class BookController : Controller
+    {
+        const string IMG_FOLDER = "images";
+        readonly BookCartDbContext _ctx;
+        readonly IWebHostEnvironment _env;
+
+        public BookController(BookCartDbContext ctx, IWebHostEnvironment env)
+        {
+            _ctx = ctx;
+            _env = env;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(BookCreateDto book)
+        {
+            if (ModelState.IsValid)
+            {
+                Book item = new Book
+                {
+                    Title = book.Title,
+                    Description = book.Description,
+                    Price = book.Price,
+                    PriceDiscount = book.PriceDiscount,
+                };
+                if (book.Image != null && book.Image.Length > 0)
+                {
+                    string imgFolder = Path.Combine(_env.ContentRootPath, IMG_FOLDER);
+                    if (Directory.Exists(imgFolder))
+                    {
+                        Directory.CreateDirectory(imgFolder);
+                    }
+                    string imgPath = Path.Combine(IMG_FOLDER, book.Image.FileName);
+                    using(var stream = new FileStream(imgPath, FileMode.Create))
+                    {
+                        await book.Image.CopyToAsync(stream);
+                        item.Image = book.Image.FileName;
+                    }
+                }
+                _ctx.Books.Add(item);
+                await _ctx.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+
+    }
+}
---
+++ BookCart/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookCart.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        [Column(TypeName="money")]
+
+        public decimal Price { get; set; }
+        [Column(TypeName ="money")]
+
+        public decimal PriceDiscount{ get; set;}
+
+        public string? Image {  get; set; }
+    
+    }
+}
